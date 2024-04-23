83036262c6e5c8f047cbef441af06a9cafb59820
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-23 17:01:53 +0900

    Update abc034_a.pyx

--- abc034_a.pyx
+++ abc034_a.pyx
@@ -1,4 +1,5 @@
-
+Cython では、Python 構文を直接使用できますが、静的型付けを使用してパフォーマンスを向上させることもできます。 
+このコードでは、cdef を使用して変数 a と b を整数として宣言しています。
 
 cdef int a, b
 
@@ -7,3 +8,5 @@ if a < b:
     print('Better')
 else:
     print('Worse')
+
+    
